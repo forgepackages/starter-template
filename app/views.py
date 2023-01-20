3944ec8a6b45145a4bcd8f16d2efffece048ab6f
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-01-19 21:24:41 -0600

    Next (#1)

--- app/views.py
+++ app/views.py
@@ -3,10 +3,44 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
-from forge.views.mixins import HTMLTitleMixin
-
 from forms import SignupForm
 from teams.models import Team, TeamMembership, TeamRoles
+
+
+class HTMLTitleMixin:
+    html_title = ""
+    html_title_prefix = ""
+    html_title_suffix = ""
+    html_title_required = True
+
+    def get_html_title(self):
+        """
+        Return the class title attr by default,
+        but can customize this by overriding
+        """
+        return self.html_title
+
+    def get_html_title_required(self):
+        return self.html_title_required
+
+    def get_html_title_prefix(self):
+        return self.html_title_prefix
+
+    def get_html_title_suffix(self):
+        return self.html_title_suffix
+
+    def generate_html_title(self):
+        title = self.get_html_title()
+
+        if not title and self.get_html_title_required():
+            raise ValueError("HTMLTitleMixin requires an html_title")
+
+        return self.get_html_title_prefix() + title + self.get_html_title_suffix()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["html_title"] = self.generate_html_title()
+        return context
 
 
 class BaseLoggedInMixin(LoginRequiredMixin, HTMLTitleMixin):
